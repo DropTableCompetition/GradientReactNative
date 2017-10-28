c1043193313e8ede7c5393d9ca484f5b8e49c736
diamondrubix <Diamondrubix@gmail.com>
2017-10-28 03:50:53 -0500

    test

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import {AppRegistry, Text, Image, View, StyleSheet,TextInput, ListView, Alert,Button,Touchable,ScrollView} from 'react-native';
+import { TabNavigator } from "react-navigation";
+import mainPage from './mainPage.js';
+import Profile from './Profile.js';
+import allPost from './AllPost.js';
+//import socket from './utility/socketManager.js'
+var poster = require('./Poster.js');
 
-export default class HelloWorldApp extends Component {
-    render() {
-        return (
-            <Text>Hello world!</Text>
-        );
-    }
-}
+
+const MainScreenNavigator = TabNavigator({
+    Profile: { screen: Profile},
+    main: { screen: mainPage },
+    All: { screen: allPost },
+},  {tabBarOptions: {
+    activeTintColor: '#fefdff',
+    activeBackgroundColor: '#8e46c9',
+    labelStyle: {
+        fontSize: 12,
+    },
+    style: {
+        height : 0,
+        opacity: 0,
+    },
+}});
+
+
+MainScreenNavigator.navigationOptions = {
+    title: 'My Chats',
+};
+
+module.exports = MainScreenNavigator;
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('GradientReactNative', () => MainScreenNavigator);
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('GradientReactNative', () => FlexDimensionsBasics);
